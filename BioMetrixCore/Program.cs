aa6dfdb4ab25208407ba09955e04827ff08d1433
ShiamMisbah <shiam.misbah42@gmail.com>
2022-08-29 14:24:17 +0600

    Option added to write logs.

--- BioMetrixCore/Program.cs
+++ BioMetrixCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Windows.Forms;
 
 namespace BioMetrixCore
@@ -8,9 +11,24 @@ namespace BioMetrixCore
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// 
+        public static void writeToFile(string content)
+        {
+            string path = @"E:\Gate\out.txt";
+            File.AppendAllText(path, content+"\n");
+        }
+
+        public static void writeToFileWithoutNL(string content)
+        {
+            string path = @"E:\Gate\out.txt";
+            File.AppendAllText(path, content);
+        }
+
         [STAThread]
         static void Main()
         {
+            
+            writeToFile("Started+++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Master());
