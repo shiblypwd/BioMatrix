90008fdc82d573a56068069bdf86cedd5355b7ed
ShiamMisbah <shiam.misbah42@gmail.com>
2022-09-12 10:42:25 +0600

    UserEntry.cs

---
+++ BioMetrixCore/Utilities/UserEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BioMetrixCore.Utilities
+{
+    public class UserEntry
+    {
+        public int Id { get; set; }
+        //public string Name { get; set; }
+        public string DataStr { get; set; } 
+        public DateTime EntryTime { get; set; }
+
+        public UserEntry(int id, string dataStr, DateTime entryTime)
+        {
+            Id=id;
+            DataStr=dataStr;
+            EntryTime=entryTime;
+        }
+    }
+}
