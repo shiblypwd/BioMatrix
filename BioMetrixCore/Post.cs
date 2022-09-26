ada08b9c25a73dd57dbf0d4b9b1012f95e51e480
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-09-26 14:05:10 +0600

    Post Class

---
+++ BioMetrixCore/Post.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BioMetrixCore
+{
+    public class Post
+    {
+        public int id { get; set; }
+        public int attendanceCardId { get; set; }
+
+        public DateTime entryTime { get; set; }
+        public string machineName { get; set; }
+        public string verifyMode    { get; set; }
+        public int inOutStatus { get; set; }
+    }
+}
