3b51b5a7bd6be2e1ed3845bdb9dde69e0ece5089
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-09-29 12:16:59 +0600

    Post ekta ya.

--- BioMetrixCore/AlphaSMS.cs
+++ BioMetrixCore/AlphaSMS.cs
@@ -1,11 +1,10 @@
 ﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RestSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BioMetrixCore
 {
@@ -43,45 +42,48 @@ namespace BioMetrixCore
             }
         }
 
-        async public void insertEntry()
+         public void insertEntry()
         {
             try
             {
-
-                HttpClient client = new HttpClient();
-                var requestInput = new Dictionary<string, string>
-            {
-                {"id", "10"},
-                {"attendanceCardId", "0"},
-                {"entryTime", "2022-09-26T06:35:17.149Z"},
-                {"machineName", "InGate-22"},
-                {"verifyMode", "Card"},
-                {"inOutStatus", "1"}
-            };
+                string url = "https://localhost:44399/api/app/attendance";
+               //string url = "https://jsonplaceholder.typicode.com/posts";
 
 
-                //requestInput.Add("msg", body);
-                //requestInput.Add("to", mobileNo);
+                var client = new RestClient(url);
 
-                string url = "http://localhost:44399/Home/Create";
-                var data = new FormUrlEncodedContent(requestInput);
-                var httpResponse = client.PostAsync(url, data);
+                var request = new RestRequest(url);
+
+                var body = new Post {
+                    id = 1,
+                    attendanceCardId = 5,
+                    entryTime = DateTime.Now,
+                    machineName = "InGate-22",
+                    verifyMode = "Card",
+                    inOutStatus=0
+                };
+
+                request.AddJsonBody(body);
+
+                var response = client.Post(request);
+
+                Console.WriteLine(response.StatusCode.ToString() + "  " + response.Content.ToString());
+
+                Console.Read();
+
             }
             catch (Exception ex)
             {
-
+                    //{ "id", "10"},
+                    //{ "attendanceCardId", "0"},
+                    //{ "entryTime", "2022-09-26T06:35:17.149Z"},
+                    //{ "machineName", "InGate-22"},
+                    //{ "verifyMode", "Card"},
+                    //{ "inOutStatus", "1"}
                 throw;
             }
-            //var httpResponse = await client.PostAsync(url, data);
-
-            //if (httpResponse.Content != null)
-            //{
-            //    var responseContent = await httpResponse.Content.ReadAsStringAsync();
-            //    dynamic response = JObject.Parse(responseContent);
-            //    Console.WriteLine("Status: " + response.data.request_status);
-            //    Console.WriteLine("ErrorMessage: " + response.data.error);
-
-            //}
+            
+            
         }
 
         async public void postRequest(Post postData)
